b71e5ad5f1c2f799bc89ad947863b781af0f64fc
KomaR KomaR <kluev2001@yandex.ru>
2025-01-20 22:39:15 +0300

    add endpoint

--- STK/Controllers/OrganizationsController.cs
+++ STK/Controllers/OrganizationsController.cs
@@ -2,11 +2,12 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using STK.Application.DTOs;
+using STK.Application.DTOs.ListOrganizations;
 using STK.Application.Queries;
 
 namespace STK.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/organizations")]
     [ApiController]
     public class OrganizationsController : ControllerBase
     {
@@ -16,11 +17,24 @@ namespace STK.API.Controllers
         {
             _mediator = mediator;
         }
+
         [HttpGet]
-        public async Task<ActionResult<List<OrganizationDto>>> GetAllOrganizations()
+        public async Task<ActionResult<List<ConciseOrganizationsDto>>> GetAllOrganizations()
         {
             var organizations = await _mediator.Send(new GetOrganizationsQuery());
             return Ok(organizations);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OrganizationDto>> GetOrganizationById(Guid id)
+        {
+            var request = new GetOrganizationByIdQuery(id);
+            var organization = await _mediator.Send(request);
+            if(organization == null)
+            {
+                return NotFound();
+            }   
+            return Ok(organization);
         }
     }
     
