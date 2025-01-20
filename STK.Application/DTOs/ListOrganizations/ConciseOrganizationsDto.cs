3ab1202c1e1fc0a57e1f6a491492177066da7927
KomaR KomaR <kluev2001@yandex.ru>
2025-01-20 22:40:22 +0300

    add new dto

---
+++ STK.Application/DTOs/ListOrganizations/ConciseOrganizationsDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STK.Application.DTOs.ListOrganizations
+{
+    public class ConciseOrganizationsDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public string Adress { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Website { get; set; }
+        public ConciseRequisiteDto ConciseRequisite { get; set; }
+        public List<EconomicActivityDto> EconomicActivities { get; set; }
+    }
+}
